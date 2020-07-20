14e48698773305e1582d94413591a8dc300843b1
IharY <igor.yanushkevich@gmail.com>
2020-07-20 23:22:37 +0300

    tra-la-la

--- src/by/ita/yanush/les03/Task01.java
+++ src/by/ita/yanush/les03/Task01.java
@@ -2,7 +2,7 @@ package by.ita.yanush.les03;
 
 public class Task01 {
     public static void main(String[] args) {
-        System.out.println();
+        System.out.println("Kellp Igor");
 
     }
 }
