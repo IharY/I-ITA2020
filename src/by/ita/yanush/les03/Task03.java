947dbbb1d8b6f12d5ea3461f72d1a6bffd68a705
IharY <igor.yanushkevich@gmail.com>
2020-07-22 05:50:14 +0300

    tra-la-la

--- src/by/ita/yanush/les03/Task03.java
+++ src/by/ita/yanush/les03/Task03.java
@@ -1,4 +1,23 @@
 package by.ita.yanush.les03;
 
+/*
+Дана последовательность действительных чисел а1, а2, а3 ,..., аn. Выяснить, будет ли она возрастающей.
+ */
+
 public class Task03 {
+    public static void main(String[] args) {
+        int [] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9};
+        int pointer = 0;
+        for(int i = 1; i < array.length; i++){
+            if(array[i-1] > array[i]){
+                pointer++;
+            }
+        }
+        if(pointer != 0){
+            System.out.println("Данная последовательность не является возрастающей");
+        }
+        else{
+            System.out.println("Данная последовательность является возрастающей");
+        }
+    }
 }
