1a8579dd5d0405ea56e40e56ac6a59cc85481745
IharY <igor.yanushkevich@gmail.com>
2020-07-24 23:25:05 +0300

    Commit

---
+++ src/by/ita/yanush/les04/Task04.java
@@ -0,0 +1,68 @@
+package by.ita.yanush.les04;
+
+/*
+Сформировать квадратную матрицу порядка n по заданному образцу (n - четное). 1   2     3   ... n
+                                                                             n n - 1 n - 2 ... 1
+                                                                             1   2     3   ... n
+                                                                          (  n n - 1 n - 2 ... 1 )
+                                                                             .   .     .    .  .
+                                                                             .   .     .    .  .
+                                                                             n n - 1 n - 2 ... 1
+ */
+
+import java.util.Random;
+
+public class Task04 {
+    public static void main(String[] args) {
+        int[][] matrix = new int[5][5];
+
+        System.out.println("Исходная матрица:");
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+
+                System.out.print(matrix[i][j] + " ");
+
+            }
+            System.out.println();
+        }
+
+
+        System.out.println();
+        System.out.println("Новая матрица: ");
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                if (i % 2 == 0) {
+
+                    matrix[i][j] = j + 1;
+                    System.out.print(matrix[i][j] + " ");
+
+                } else {
+
+                    matrix[i][j] = matrix[0].length - j;
+                    System.out.print(matrix[i][j] + " ");
+
+                }
+            }
+            System.out.println();
+        }
+        //Random rn =  new Random();
+        /*int matrix[][] = new int[][]{
+                {12, 13, 14, 5, 11},
+                {18, 2, 19, 11, 13},
+                {10, 16, 12, 14, 7},
+                {19, 9, 21, 10, 17},
+                {11, 12, 22, 15, 9}};*/
+
+        /*for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+
+                matrix[i][j] = rn.nextInt(1000);
+
+            }
+            System.out.println();
+        }*/
+
+    }
+}
---
+++ src/by/ita/yanush/les04/Task05.java
@@ -0,0 +1,44 @@
+package by.ita.yanush.les04;
+
+/*
+Сформировать квадратную матрицу порядка n по заданному образцу (n - четное). 0   0   0   ...  0  0  1
+                                                                             0   0   0   ...  0  2  0
+                                                                             0   0   0   ...  3  0  0
+                                                                             .   .   .    .   .  .  .
+                                                                          (  .   .   .    . . .  .  .  )
+                                                                             .   .   .    .   .  .  .
+                                                                             0 n - 1 0   ...  0  0  0
+                                                                             n   0   0   ...  0  0  0
+ */
+
+public class Task05 {
+    public static void main(String[] args) {
+        int[][] matrix = new int[5][5];
+
+        System.out.println("Исходная матрица:");
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+
+                System.out.print(matrix[i][j] + " ");
+
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+        System.out.println("Новая матрица: ");
+        for (int i = 0; i < matrix.length; i++){
+            matrix[i][matrix.length - 1 - i] = i + 1;
+        }
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+
+                System.out.print(matrix[i][j] + " ");
+
+            }
+            System.out.println();
+        }
+    }
+}
