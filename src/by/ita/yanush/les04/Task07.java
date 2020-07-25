610bffdff570df752cb113d2005bbdbc2d0d260f
IharY <igor.yanushkevich@gmail.com>
2020-07-25 14:32:36 +0300

    Commit

---
+++ src/by/ita/yanush/les04/Task07.java
@@ -0,0 +1,61 @@
+package by.ita.yanush.les04;
+
+/*
+Сформировать квадратную матрицу порядка n по заданному образцу (n - четное). 1    1    1    ...   1   1   1
+                                                                             2    2    2    ...   2   2   0
+                                                                             3    3    3    ...   3   0   0
+                                                                             .    .    .     .    .   .   .
+                                                                          (  .    .    .     .  . .   .   .  )
+                                                                             .    .    .     .    .   .   .
+                                                                          n - 1 n - 1  0    ...   0   0   0
+                                                                             n    0    0    ...   0   0   0
+ */
+
+public class Task07 {
+    public static void main(String[] args) {
+        int[][] matrix = new int[5][5];
+        int breaker = 4;
+
+        System.out.println("Исходная матрица:");
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+
+                System.out.print(matrix[i][j] + " ");
+
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+        System.out.println("Новая матрица: ");
+        /*for (int i = 0; i < matrix.length; i++){
+            for (int j = matrix[i].length - 1; j >= 0; j--) {
+
+                //matrix[i][matrix.length - 1 - i] = i + 1;
+                System.out.print(matrix[i][j - i] = i + 1);
+            }
+        }*/
+        for (int i = 0; i < matrix.length; i++){
+            for (int j = 0; j < matrix[0].length; j++) {
+                matrix[i][j] = i + 1;
+
+                if (j == breaker){
+                    breaker --;
+                    break;
+
+                }
+
+            }
+        }
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+
+                System.out.print(matrix[i][j] + " ");
+
+            }
+            System.out.println();
+        }
+    }
+}
