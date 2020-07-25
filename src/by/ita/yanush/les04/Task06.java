285f359b4da24c94598e43e2d005053f39dc7021
IharY <igor.yanushkevich@gmail.com>
2020-07-25 12:42:36 +0300

    Commit

---
+++ src/by/ita/yanush/les04/Task06.java
@@ -0,0 +1,52 @@
+package by.ita.yanush.les04;
+
+/*
+Сформировать квадратную матрицу порядка n по заданному образцу (n - четное). 1   1   1   ...  1  1  1
+                                                                             1   0   0   ...  0  0  1
+                                                                             1   0   0   ...  0  0  1
+                                                                             .   .   .    .   .  .  .
+                                                                          (  .   .   .    . . .  .  .  )
+                                                                             .   .   .    .   .  .  .
+                                                                             1   0   0   ...  0  0  1
+                                                                             1   1   1   ...  1  1  1
+ */
+
+public class Task06 {
+    public static void main(String[] args) {
+        int[][] matrix = new int[5][5];
+
+        System.out.println("Исходная матрица:");
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+
+                System.out.print(matrix[i][j] + " ");
+
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+        System.out.println("Новая матрица: ");
+
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                if (i == matrix.length - 1 || j == matrix[0].length - 1 || i == 0 || j == 0) {
+                    matrix[i][j] = 1;
+                }
+
+            }
+
+        }
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+}
+
